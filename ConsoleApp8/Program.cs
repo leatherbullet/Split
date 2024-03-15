5c98a7c41b8402b114c27c4bfdccd4b77db6493c
leatherbullet <al.obnizov@yandex.ru>
2024-03-15 10:30:24 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Учиться, учится и еще раз учится.";
+            char[] separators = new char[] { ' ', ',', '.' };
+
+            string[] words = text.Split (separators,StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string word in words)
+            {
+                Console.WriteLine($"подстрока: {word}");
+            }
+        }
+    }
+}
